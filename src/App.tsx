5538420425670b5bade14397c0c0e6ca49338f32
Erick <erickbaddog2@gmail.com>
2022-06-21 01:19:08 -0300

    connected api with apollo and graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from "react";
-import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+interface Lesson{
+  title: string,
+  id: string
 }
+
+const App = () => {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+  
+  return (
+    <div>
+      <ul>
+        {data?.lessons.map((lesson) => (
+          <li key={lesson.id}>{lesson.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
